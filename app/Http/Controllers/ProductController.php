6f18e7cb3b0cd14b4b97ad08c1de0f64450ec620
Lic. Leonardo Manuel Tito <leonardo_tito@yahoo.com.ar>
2021-05-24 21:22:55 -0300

    Componentes esenciales de Laravel.

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    public function create()
+    {
+        return 'This is the form to create a product';
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($product)
+    {
+        return view('products.show');
+    }
+
+    public function edit($product)
+    {
+        return "Showing the form to edit the product with id {$product}";
+    }
+
+    public function update($product)
+    {
+        //
+    }
+
+    public function destroy($product)
+    {
+        //
+    }
+}
